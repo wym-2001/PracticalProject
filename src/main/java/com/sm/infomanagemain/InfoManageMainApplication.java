61b3375f3f6d8bbc222d6987d418fca36d2de640
wym-2001 <138110603+wym-2001@users.noreply.github.com>
2023-06-29 18:44:44 +0800

    Add files via upload

---
+++ src/main/java/com/sm/infomanagemain/InfoManageMainApplication.java
@@ -0,0 +1,13 @@
+package com.sm.infomanagemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoManageMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoManageMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sm/infomanagemain/InfoManageMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.infomanagemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoManageMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
